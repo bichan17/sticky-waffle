27e951370aaebdf0ed94f0e812103bcaebdcac9f
eric <eric.bichan@gmail.com>
2013-03-24 16:40:28 -0400

    added oscillator and controls

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,69 @@
 
 
 app.main = (function(){
+	var aContext;
+	var oscillator;
+	var started = false;
 	function init (){
 		console.log("initialized!");
+		aContext = new webkitAudioContext();
+		oscillator = aContext.createOscillator();
+
+		console.log("setup UI");
+		var startButton = document.querySelector("#start");
+		var stopButton = document.querySelector("#stop");
+		var slider = document.querySelector("#slider1");
+		var wavetype = document.querySelector("#wavetypeSelect");
+
+		startButton.addEventListener("click", function(){
+			started = true;
+			startSound(wavetype.value, slider.value);
+		});
+
+		stopButton.addEventListener("click", function(){
+			started = false;
+			stopSound();
+		});
+
+		slider.addEventListener("change", function(e){
+		 	//console.log("checked=" + e.target.value);
+		 	 document.querySelector("#sliderResults").innerHTML = e.target.value;
+
+		 	 startSound(wavetype.value, e.target.value);
+			
+		 });
+		
+		wavetype.addEventListener("change", function(e){
+		 	//console.log("checked=" + e.target.value);
+		 	 
+		 	 startSound(e.target.value, slider.value);
+			
+		 });
+		
+		
+
+
+	}
+
+
+	function startSound(type, fq){
+		console.log("start");
+		// oscillator.stop(0);
+		if(started == true){
+			oscillator.type = type;
+			oscillator.frequency.value = fq;
+			oscillator.connect(aContext.destination);
+			oscillator.start(0);
+		}
+
+		
+	}
+
+	function stopSound(){
+		console.log("stop");
+
+		oscillator.stop(0);
+		oscillator = aContext.createOscillator();
 	}
 
 	//Public interface
