bb11ba0612e7f241a266796efdbc6b27aa67e054
luis <lrosario101@gmail.com>
2013-03-24 19:36:31 -0400

    ball is moving

--- scripts/visualizer.js
+++ scripts/visualizer.js
@@ -2,16 +2,37 @@ app.visualizer = (function() {
   // globals
   var canvas,
       ctx;
+      // _X,
+      // _Y;
+  var _X = 0,
+      _Y = window.innerHeight/2;
 
   function Visualizer (_canvas, mode) {
     console.log(_canvas);
     canvas = _canvas;
+    ctx = canvas.getContext("2d");
 
     // visualizer should take up the whole space
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
-
     window.onresize = resizeCanvas;
+
+    // the starting position will be the middle of the canvas
+    // ctx.translate();
+
+    window.requestAnimFrame = (function(){
+      return  window.requestAnimationFrame       ||
+              window.webkitRequestAnimationFrame ||  
+              window.mozRequestAnimationFrame    ||  
+              window.oRequestAnimationFrame      ||  
+              window.msRequestAnimationFrame     ||  
+              function( callback ){ 
+                window.setTimeout(callback, 1000 / 60); 
+              };
+    })();
+
+    // the good stuff
+    draw();
   }
 
   return {
@@ -22,6 +43,63 @@ app.visualizer = (function() {
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
   }
+
+  function draw() {
+
+    // var dt = calculateDeltaTime();
+    // if(!paused) {
+      // 1. update sprites
+      update();
+      // 2. draw bkgd
+      drawBackground();
+      // 3. everything else
+      drawSprites();
+    // }
+    // is the browser ready to draw?
+    window.requestAnimFrame(draw);
+
+
+    //  ctx.arc(_x, _y, _radius,startAngle,endAngle, clockwise)
+    // var _X = window.innerWidth/2,
+    //     _Y = window.innerHeight/2;
+    // console.log('lol');
+    // ctx.fillStyle = "#f00";
+    // ctx.beginPath();
+    // ctx.arc(_X, _Y, 20, 0, Math.PI*2, false);
+    // ctx.closePath();
+    // ctx.fill();
+    // ctx.stroke();
+  }
+
+  function update() {
+    // console.log("x: "+_X);
+    // console.log("y: "+_Y);
+    _X += 4;
+    if (_X > window.innerWidth) _X = 0;
+    // _Y = 20 + 30* Math.sin(_X/50);
+    // _Y = y-position + amplitude * waveLength 
+    _Y = (window.innerHeight/2) + 300 * Math.sin(_X/50);
+
+    // console.log("x2: "+_X);
+    // console.log("y2: "+_Y);
+  }
+  function drawBackground() {
+    // console.log("2");
+    canvas.width = canvas.width;
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);  
+  }
+  function drawSprites() {
+    // console.log("3");
+    ctx.fillStyle = "#f00";
+    ctx.beginPath();
+    ctx.arc(_X, _Y, 20, 0, Math.PI*2, false);
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+  }
+
+
+
 
 // background(0, 0, 0);
 // noStroke();
@@ -51,13 +129,6 @@ app.visualizer = (function() {
 //     x += 4;
 //     y = 200 + 30*sin(x/50);
 // };
-
-// console.log(app.main.viz);
-
-// app.main.viz;
-
-
-// console.log('lol <-- from: visualizer.js');
 
 
 
