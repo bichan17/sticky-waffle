598b6abc3e1eec31b587e5f5a679b80b1419f88c
luis <lrosario101@gmail.com>
2013-04-01 19:39:03 -0400

    minor adds

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,13 @@
 
 app.main = (function(){
 	var synth;
+	var viz;
+	var viz_settings; // the "visualizer" object (holds settings)
+	var body;
 
 	function init (){
 		console.log("initialized!");
+		body = document.getElementsByTagName('body')[0];
 
 		// SYNTH SETUP
 		console.log("setup UI");
@@ -18,14 +22,52 @@ app.main = (function(){
 		synth = new app.synth.Synth(startButton, stopButton, frequencyControl, wavetypeControl);
 
 		// VISUALIZER SETUP
-		viz = new app.visualizer.Visualizer(
-			document.getElementById('visualizer'),
-			synth
-		);
+		// ----------------------------
+		// 1. create the viz_settings object
+		viz_settings = {};
+		// 2. every change updates the viz_settings objects
+		// add event listeners to Color buttons
+		var colorButtons = document.getElementById('bkgd').getElementsByTagName('input');
+		for (var i = 0; i < colorButtons.length; i++) {
+			colorButtons[i].addEventListener("change", changeColor, false);
+			// get the default checked value
+			if (colorButtons[i].checked){viz_settings.color = colorButtons[i].value;}
+		}
+		// add event listeners to Mode buttons
+		var modeButtons = document.getElementById('mode').getElementsByTagName('input');
+		for (var j = 0; j < modeButtons.length; j++) {
+			modeButtons[j].addEventListener("change", changeVizMode, false);
+			// get the default checked value
+			if (modeButtons[j].checked){viz_settings.mode = modeButtons[j].value;}
+		}
+		// create Visualizer | pass in: DOM <canvas> reference, synth object, settings
+		viz = new app.visualizer.Visualizer(document.getElementById('visualizer'), synth, viz_settings);
+
+		// finish UI set up
+		// ----------------------------
+		body.className += 'black';
 	}
+
+	function changeColor(e) {
+		// change <canvas> colors
+		viz_settings.color = e.target.value;
+		viz.changeBackground(viz_settings.color);
+
+		// change DOM colors
+		body.className = ''; // clear classNames
+		if (viz_settings.color === '#000') body.className += 'black';
+		else if (viz_settings.color === '#fff') body.className += 'white';
+	}
+
+	function changeVizMode(e) {
+		viz_settings.mode = e.target.value;
+		viz.changeMode(viz_settings.mode);
+	}
+
+
 	//Public interface
 	return{
 		init : init
 		//someFunc : someFunc
-	}
+	};
 })();
